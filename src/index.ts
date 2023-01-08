65a9c8d27a5504ab343ad2892b316d35f3d0aae4
wenay12 <wenayr@gmail.com>
2023-01-08 15:08:22 +0300

    106

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 
-// import * as baseTypes from "./Common/BaseTypes";
+import * as baseTypes from "./Common/BaseTypes";
 // import * as common from "./Common/Common";
 // import * as time from "./Common/Time";
-// import * as color from "./Common/Color";
-// import * as math from "./Common/Math";
+import * as color from "./Common/Color";
+import * as math from "./Common/Math";
 // import * as list from "./Common/List";
-// import * as listNodeAnd from "./Common/ListNodeAnd";
+import * as listNodeAnd from "./Common/ListNodeAnd";
 // // export {common}
 // // export * from "./and/CParams";
 //
 //
-// // export * from "./Common/BaseTypes";
+export * from "./Common/BaseTypes";
 // export * from "./Common/Common";
 // export * from "./Common/Time";
-// export * from "./Common/Color";
-// export * from "./Common/Math";
+export * from "./Common/Color";
+export * from "./Common/Math";
 // export * from "./Common/List";
-// export * from "./Common/ListNodeAnd";
+export * from "./Common/ListNodeAnd";
 //
 // export namespace test{
 //
