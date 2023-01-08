2db554bd745a35fbeee26b52247c93cc1882ff50
wenay12 <wenayr@gmail.com>
2023-01-08 14:33:49 +0300

    106

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,31 @@
 
-import * as baseTypes from "./Common/BaseTypes";
-import * as common from "./Common/Common";
-import * as time from "./Common/Time";
-import * as color from "./Common/Color";
-import * as math from "./Common/Math";
-import * as list from "./Common/List";
-import * as listNodeAnd from "./Common/ListNodeAnd";
-// export {common}
-// export * from "./and/CParams";
+// import * as baseTypes from "./Common/BaseTypes";
+// import * as common from "./Common/Common";
+// import * as time from "./Common/Time";
+// import * as color from "./Common/Color";
+// import * as math from "./Common/Math";
+// import * as list from "./Common/List";
+// import * as listNodeAnd from "./Common/ListNodeAnd";
+// // export {common}
+// // export * from "./and/CParams";
+//
+//
+// // export * from "./Common/BaseTypes";
+// export * from "./Common/Common";
+// export * from "./Common/Time";
+// export * from "./Common/Color";
+// export * from "./Common/Math";
+// export * from "./Common/List";
+// export * from "./Common/ListNodeAnd";
+//
+// export namespace test{
+//
+//     //Common
+//
+// }
+//
 
 
-// export * from "./Common/BaseTypes";
-export * from "./Common/Common";
-export * from "./Common/Time";
-export * from "./Common/Color";
-export * from "./Common/Math";
-export * from "./Common/List";
-export * from "./Common/ListNodeAnd";
-
-export namespace test{
-
-    //Common
-
+export function test() {
+    console.log("lalalal")
 }
-
