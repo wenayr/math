30dec2b65f8b1457f65910c6b8a9d5595c124336
wenay12 <wenayr@gmail.com>
2023-01-08 16:29:49 +0300

    1 0 10

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@
 // import * as baseTypes from "./Common/BaseTypes";
 // import * as common from "./Common/Common";
 // import * as time from "./Common/Time";
-import * as color from "./Common/Color";
 // import * as math from "./Common/Math";
 // import * as list from "./Common/List";
+import * as color from "./Common/Color";
 import * as listNodeAnd from "./Common/ListNodeAnd";
+// import * as console from "console";
+import {CListNodeAnd} from "./Common/ListNodeAnd";
 // // export {common}
 // // export * from "./and/CParams";
 //
@@ -13,19 +15,16 @@ import * as listNodeAnd from "./Common/ListNodeAnd";
 // export * from "./Common/BaseTypes";
 // export * from "./Common/Common";
 // export * from "./Common/Time";
-export * from "./Common/Color";
+// export * from "./Common/Color";
+// export * from "./Common/ListNodeAnd";
 // export * from "./Common/Math";
 // export * from "./Common/List";
-export * from "./Common/ListNodeAnd";
 //
-// export namespace test{
-//
-//     //Common
-//
-// }
 //
 
 
 export function test() {
+    const tt = new CListNodeAnd()
+    console.log("test");
     console.log("lalalal")
 }
