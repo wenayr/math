18f78b5c373e375382eec64bcd45968c3e877705
wenay12 <wenayr@gmail.com>
2024-08-23 15:57:51 +0300

    funcListenCallback

--- src/Common/Listen.ts
+++ src/Common/Listen.ts
@@ -18,6 +18,7 @@ export function funcListenCallback<T extends any[]>(a: (e: tr222<T>)=>(void | ((
         addListen: (a: tr222<T>) => {
             obj.set(a, a)
             event?.("add", obj.size, api)
+            return () => api.removeListen(a)
         },
         removeListen: (a: (tr222<T>)|null) => {
             obj.delete(a)
@@ -167,6 +168,7 @@ export function PromiseArrayListen<T extends any = unknown>(array: ((() => Promi
         },
         promise: {
             all: () => Promise.all(arr),
+
             allSettled: () => Promise.allSettled(arr),
         },
         getData(){return arr},
@@ -174,4 +176,53 @@ export function PromiseArrayListen<T extends any = unknown>(array: ((() => Promi
             return {ok, error, count}
         }
     }
+}
+
+
+export type realSocket2<T extends any> = (data: {callback: (data: T)=>void, [key: string]: any}, ...b: any[]) => (any |(()=>any))
+export type getTypeCallback<T extends realSocket2<any>> = T extends realSocket2<infer R> ? R : never
+type ParametersOther<T extends (forget: any,...args: any) => any> = T extends (forget: any,...args: infer P ) => any ? P : never;
+
+type tr22<T> = T extends undefined ? never : T
+export function socketBuffer3<T extends realSocket2<any| any[]>, T2 extends (readonly unknown[])|undefined, T3 extends {[key: string]: unknown}>(
+    func: T,
+    callbackMain: (data: getTypeCallback<T>, memo: T3)=> T2,
+    memo: T3 = {} as T3
+) {
+    return (a: Omit<Parameters<T>[0],"callback"> & {callback: (...data: tr22<T2>)=> any}, ...b:  ParametersOther<T>) =>
+        func({...a, callback: (v)=>{const z = callbackMain(v, memo); if (z) a.callback(...(z as tr22<T2>))} //  as T
+        }, ...b) as ReturnType<T>
+}
+
+function socketBufferBySnapshot<T extends realSocket2<any| any[]>, T2 extends (readonly unknown[])|undefined, T3 extends {[key: string]: unknown}>({func, memo = {} as T3, callbackSave, snapshot}: {
+    func: ()=>T,
+    callbackSave: (data: getTypeCallback<T>, memo: T3) => T2,
+    memo: T3,
+    snapshot?: (memo: T3) => T3                                                                                                                                       }
+) {
+    const f = socketBuffer3(func(), callbackSave, memo)
+    type tt = typeof socketBuffer3<T, T2, T3>
+
+    let d: ReturnType<tt>|null
+    const [callback, listenA] = UseListen<Parameters<typeof callbackSave>>({
+        event: (type, count, api) => {
+        if (type == "remove" && count == 0) api.close()
+        if (type == "add" && count == 1) api.run()
+        }});
+    const connect = () => {
+        if (d == null) {
+            d = socketBuffer3(func(), (...a)=>{
+                callback(...a)
+                return callbackSave(...a)
+            }, memo)
+            return d
+        }
+    }
+    const run = (...params: Parameters<typeof listenA.addListen>)=> {
+        if (!listenA.isRun()) {
+            snapshot?.(memo)
+            connect()
+        }
+        return listenA.addListen(...params)}
+    return {run, snapshot, memo, listenA, connect, get disconnect(){return d}}
 }
